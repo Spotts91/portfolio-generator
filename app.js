a097059009fe082f33f834ddcd706137c965364f
Spotts91 <oakshassa864@gmail.com>
2022-12-07 12:10:54 -0800

    user input and HTML file creation

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html>
+    <head>
+        <meta charse="UTF-8">
+        <meta name-"viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>Portfolio Demo</title>
+    </head>
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">GitHub</a></h2>
+    </body>
+    </html>
+    `;
 };
 
-printProfileData(profileDataArgs);
+console.log(name, github);
+console.log(generatePage(name, github));
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
