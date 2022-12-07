37516f2797ad035a7109e768c6a0d0e57b255f3c
Spotts91 <oakshassa864@gmail.com>
2022-12-07 12:46:55 -0800

    HTML file generation and user input logic

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-const [name, github] = profileDataArgs;
 const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html>
-    <head>
-        <meta charse="UTF-8">
-        <meta name-"viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie-edge">
-        <title>Portfolio Demo</title>
-    </head>
+const profileDataArgs = process.argv.slice(2);
 
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">GitHub</a></h2>
-    </body>
-    </html>
-    `;
-};
+const [name,github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
